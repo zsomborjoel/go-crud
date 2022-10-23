1c54703a6c9d7af294c24afdb8d3653319f6eb88
Zsombor Joel Gyurkovics <zsomborjoel@gmail.com>
2022-10-23 20:49:23 +0200

    Change main import to internal

--- cmd/go-crud/main.go
+++ cmd/go-crud/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/zsomborjoel/go-crud/pkg/initializers"
-	"github.com/zsomborjoel/go-crud/pkg/routes"
+	"github.com/zsomborjoel/go-crud/internal/initializers"
+	"github.com/zsomborjoel/go-crud/internal/routes"
 )
 
 func init() {
