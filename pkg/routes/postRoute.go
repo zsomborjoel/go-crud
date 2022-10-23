2713ffa273c85fe5d2b744186df619885d1a70d6
Zsombor Joel Gyurkovics <zsomborjoel@gmail.com>
2022-10-23 20:05:07 +0200

    Make controller to handler

--- pkg/routes/postRoute.go
+++ pkg/routes/postRoute.go
@@ -2,13 +2,13 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/zsomborjoel/go-crud/pkg/controllers"
+	"github.com/zsomborjoel/go-crud/pkg/handlers"
 )
 
 func PostsRegister(router *gin.RouterGroup) {
-	router.POST("", controllers.PostsCreate)
-	router.POST("/:id", controllers.PostsUpdate)
-	router.GET("", controllers.PostsIndex)
-	router.GET("/:id", controllers.PostsShow)
-	router.DELETE("/:id", controllers.PostsDelete)
+	router.POST("", handlers.PostsCreate)
+	router.POST("/:id", handlers.PostsUpdate)
+	router.GET("", handlers.PostsIndex)
+	router.GET("/:id", handlers.PostsShow)
+	router.DELETE("/:id", handlers.PostsDelete)
 }
